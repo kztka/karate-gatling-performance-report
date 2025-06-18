c1e16ba05a285b34642d00cbd38a369ed4b11829
kztka <kzk_taka_clar01@yahoo.co.jp>
2025-06-18 10:12:25 +0900

    first commit

---
+++ js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1750167925000,8],[1750167926000,7],[1750167927000,8],[1750167928000,8],[1750167929000,5],[1750167930000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
